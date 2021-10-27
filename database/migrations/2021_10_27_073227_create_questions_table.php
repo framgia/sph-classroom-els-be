d9d5665096fef21457ceb26a2dccca6f3ba31e1a
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2021-10-27 16:03:30 +0800

    E_CLASSROOM-93 [API] [Migration] Initial migration files for the application

---
+++ database/migrations/2021_10_27_073227_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('quiz_id');
+            $table->foreignId('question_type_id');
+            $table->string('question');
+            $table->string('image');
+            $table->integer('time_limit');
+            $table->string('text_answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
