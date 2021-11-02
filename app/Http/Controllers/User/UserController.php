1bf24ca88e833d209bd0709ae8411ef4bfe69178
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2021-11-02 13:14:14 +0800

    E_CLASSROOM-109 [API] User Profile Functionality

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display the specified User.
+     *
+     * @param  User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response()->json($user, 200);
+    }
+
+    /**
+     * Update the specified User in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required'
+        ]);
+
+       $user->fill($request->only([
+            'name',
+            'email'
+        ]));
+
+        if($user->isClean()) {
+            return response()->json(['error' => 'You need to specify a different value to update', 'code' => 422], 422);
+        }
+
+        $user->save();
+
+        return response()->json($user, 200);
+    }
+}
