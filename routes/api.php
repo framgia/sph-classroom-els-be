6adda37ec4a26980782726a3465e31d08d936736
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2021-11-05 17:41:24 +0800

    fixed conflict

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\v1\User\UserController;
 use App\Http\Controllers\API\v1\Category\CategoryController;
+use App\Http\Controllers\API\v1\Quiz\QuizController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::resource('/users', UserController::class);
     Route::resource('/categories', CategoryController::class);
+    Route::resource('/categories.quizzes', QuizController::class);
 });
+
+
