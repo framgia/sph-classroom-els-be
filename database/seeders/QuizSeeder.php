31145c9b696189384ebb7c59580c72a2d326eff5
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2021-11-08 14:45:17 +0800

    E_CLASSROOM-132 [API] [Markup] [Setup] Quiz List Page

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -17,46 +17,55 @@ class QuizSeeder extends Seeder
         $quizzes = [
             [
                 'category_id' => 1,
+                'title' => 'Web Development Basics',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
             ],[
                 'category_id' => 2,
+                'title' => 'Branches of Science',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
             ],[
                 'category_id' => 3,
+                'title' => 'Math Basic',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
             ],[
                 'category_id' => 4,
+                'title' => 'Philosophy Quiz',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
             ],[
                 'category_id' => 5,
+                'title' => 'Language Facts',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
             ],[
                 'category_id' => 6,
+                'title' => 'Introduction to Programming',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
             ],[
                 'category_id' => 7,
+                'title' => 'All About Painting',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
             ],[
                 'category_id' => 8,
+                'title' => 'Philippines History',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
             ],[
                 'category_id' => 9,
+                'title' => 'Business',
                 'instruction' => 'Answer the following questions, depending on what is asked before the time limit.',
                 'published_at' => date("Y-m-d H:i:s"),
                 'created_at' => date("Y-m-d H:i:s")
