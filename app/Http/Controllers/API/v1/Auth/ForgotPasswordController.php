78df63719fed5429b7451170e4da5593fb091a77
Joash Cañete <89382909+CaneteJoash@users.noreply.github.com>
2021-11-08 18:48:01 +0800

    E_CLASSROOM-114 [API] Password Reset (#11)
    * E_CLASSROOM-114 [API] Password Reset
    
    * E_CLASSROOM-114 [API] Password Reset

---
+++ app/Http/Controllers/API/v1/Auth/ForgotPasswordController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ForgotPasswordRequest;
+use App\Http\Requests\Auth\ResetRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Validation\ValidationException;
+
+
+class ForgotPasswordController extends Controller
+{
+    public function forgotPassword(ForgotPasswordRequest $request)
+    {
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        if ($status == Password::RESET_LINK_SENT) {
+            return $this->authResponse('The link was sent, please Check your email');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => [trans($status)],
+        ]);
+    }
+
+    public function reset(ResetRequest $request)
+    {
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+
+                $user->tokens()->delete();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        if ($status === Password::PASSWORD_RESET) {
+            return $this->authResponse('Password was reset successfully', 201);
+        }
+        return $this->errorResponse("error", 422);
+    }
+
+
+}
