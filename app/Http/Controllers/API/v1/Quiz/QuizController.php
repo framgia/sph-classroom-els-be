8f56cd7d3558004ca0b4fcb354b265d483582158
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2021-11-12 11:15:38 +0800

    E_CLASSROOM-136 [API] Quiz List Pagination

--- app/Http/Controllers/API/v1/Quiz/QuizController.php
+++ app/Http/Controllers/API/v1/Quiz/QuizController.php
@@ -19,7 +19,7 @@ class QuizController extends Controller
     {
         $quizzes = $category->quizzes;
 
-        return $this->showAll($quizzes);
+        return $this->paginatedList($quizzes);
     }
 
     /**
--- app/Traits/ResponseMessage.php
+++ app/Traits/ResponseMessage.php
@@ -4,6 +4,8 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ResponseMessage
 {
@@ -30,5 +32,37 @@ trait ResponseMessage
     protected function authResponse($array = [], $code = 200)
     {
         return $this->successResponse($array, $code);
+    }
+    protected function paginatedList(Collection $collection, $code = 200)
+    {
+        $collection = $this->paginate($collection);
+
+        return $this->successResponse(['data' => $collection], $code);
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 12;
+        if(request()->has('per_page')){
+            $perPage = (int)request()->per_page;
+        }
+
+        $results = $collection->slice(($page-1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPage()
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
     }
 }
