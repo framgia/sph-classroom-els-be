98ca5551d7c06bb1636fcc181eadc0fe086eb9d1
John Harvey <89382692+catampongan@users.noreply.github.com>
2021-11-16 14:37:04 +0800

    Feature/e classroom 121 integration student quiz question (#18)
    * E_CLASSROOM-135 [API] Category List Pagination
    
    * E_CLASSROOM-121 [Integration] [Backend] [Student] Quiz Questions
    
    Co-authored-by: John Harvey <89382692+JHarbee@users.noreply.github.com>

--- database/seeders/ChoiceSeeder.php
+++ database/seeders/ChoiceSeeder.php
@@ -36,162 +36,162 @@ class ChoiceSeeder extends Seeder
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 2,
+                'question_id' => 3,
                 'choice' => 'Genetics',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 2,
+                'question_id' => 3,
                 'choice' => 'Zoology',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 2,
+                'question_id' => 3,
                 'choice' => 'Botany',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 2,
+                'question_id' => 3,
                 'choice' => 'Biology',
 		        'is_correct' => true,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 3,
+                'question_id' => 5,
                 'choice' => 'Archimedes',
 		        'is_correct' => true,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 3,
+                'question_id' => 5,
                 'choice' => 'Plato',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 3,
+                'question_id' => 5,
                 'choice' => 'Sumerians',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 3,
+                'question_id' => 5,
                 'choice' => 'Leonardo Da Vinci',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 4,
+                'question_id' => 7,
                 'choice' => 'Saint Thomas Aquinas',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 4,
+                'question_id' => 7,
                 'choice' => 'René Descartes',
 		        'is_correct' => true,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 4,
+                'question_id' => 7,
                 'choice' => 'Aristotle',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 4,
+                'question_id' => 7,
                 'choice' => 'Locke',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 5,
+                'question_id' => 9,
                 'choice' => 'Hindi',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 5,
+                'question_id' => 9,
                 'choice' => 'Mandarin Chinese',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 5,
+                'question_id' => 9,
                 'choice' => 'English',
 		        'is_correct' => true,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 5,
+                'question_id' => 9,
                 'choice' => 'Spanish',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 6,
+                'question_id' => 11,
                 'choice' => 'Fortran',
 		        'is_correct' => true,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 6,
+                'question_id' => 11,
                 'choice' => 'C',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 6,
+                'question_id' => 11,
                 'choice' => 'C++',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 6,
+                'question_id' => 11,
                 'choice' => 'Java',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 7,
+                'question_id' => 13,
                 'choice' => 'Pablo Picasso',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 7,
+                'question_id' => 13,
                 'choice' => 'Sandro Bottic',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 7,
+                'question_id' => 13,
                 'choice' => 'Leonardo da Vinci',
 		        'is_correct' => true,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 7,
+                'question_id' => 13,
                 'choice' => 'Raphael',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 8,
+                'question_id' => 15,
                 'choice' => 'King Philip',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 8,
+                'question_id' => 15,
                 'choice' => 'Lapu-Lapu',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 8,
+                'question_id' => 15,
                 'choice' => 'Ferdinand Magellan',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 8,
+                'question_id' => 15,
                 'choice' => 'King Philip II',
 		        'is_correct' => true,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 9,
+                'question_id' => 17,
                 'choice' => 'Owner',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 9,
+                'question_id' => 17,
                 'choice' => 'Manager',
 		        'is_correct' => true,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 9,
+                'question_id' => 17,
                 'choice' => 'Businessman',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
             ],[
-                'question_id' => 9,
+                'question_id' => 17,
                 'choice' => 'Entrepreneur',
                 'is_correct' => false,
                 'created_at' => date("Y-m-d H:i:s")
