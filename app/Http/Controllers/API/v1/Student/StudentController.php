bbc89ed5a5625fc36bc560b405fae867f408f479
Joash Cañete <89382909+CaneteJoash@users.noreply.github.com>
2021-11-24 17:15:17 +0800

    E_CLASSROOM-174 [BE] Student List Page Backend Functionality [1/3] (#22)
    * E_CLASSROOM-174 [BE] Student List Page Backend Functionality [1/3]
    
    * E_CLASSROOM-174 [BE] Student List Page Backend Functionality [1/3]

---
+++ app/Http/Controllers/API/v1/Student/StudentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Student;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\DB;
+use App\Traits\Pagination;
+use App\Models\User;
+
+class StudentController extends Controller
+{
+    use Pagination;
+
+
+    public function index()
+    {
+        $id = Auth::user()->id;
+
+        $students = User::where('user_type_id', 2)
+            ->where('id', '!=', $id)
+            ->get();
+
+        return $this->paginate($students);
+    }
+}
