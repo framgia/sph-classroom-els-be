e44b99c25fbd8208da56c67571d934a562ae263d
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2021-12-07 19:10:52 +0800

    E_CLASSROOM-169 [BE] Functionality for Friends Activities

--- app/Http/Controllers/API/v1/Student/StudentActivitiesController.php
+++ app/Http/Controllers/API/v1/Student/StudentActivitiesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Spatie\Activitylog\Models\Activity;
+use Illuminate\Support\Facades\Auth;
 
 
 class StudentActivitiesController extends Controller
