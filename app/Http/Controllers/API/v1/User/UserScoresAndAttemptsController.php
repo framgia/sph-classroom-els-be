74ce63b39e6d279fe532e78075156b9927ece301
Joash Cañete <joash.canete@sun-asterisk.com>
2021-12-14 17:23:27 +0800

    E_CLASSROOM-204 [FE/BE] Make Category Quizzes page dynamic

---
+++ app/Http/Controllers/API/v1/User/UserScoresAndAttemptsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Quiz;
+
+class UserScoresAndAttemptsController extends Controller
+{
+    public function UserScoreAndAttempts(Request $request)
+    {
+        $id = Auth::user()->id;
+
+        $recent = Quiz::join('quizzes_taken', 'quizzes.id', '=',  'quizzes_taken.quiz_id',)
+            ->where('quizzes_taken.user_id', '=', $id)
+            ->where('quizzes_taken.quiz_id', '=', $request->quiz_id)
+            ->orderByDesc('quizzes_taken.created_at')
+            ->get();
+
+        return $this->successResponse(["recentQuizzesTaken" => $recent], 200);
+    }
+    
+}
