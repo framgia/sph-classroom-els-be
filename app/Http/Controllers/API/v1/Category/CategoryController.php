6b0a9c126b762501d033f6a6a91cc84e73ae9271
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2022-01-17 13:01:41 +0800

    E_CLASSROOM-211 BE Category Search Function

--- app/Http/Controllers/API/v1/Category/CategoryController.php
+++ app/Http/Controllers/API/v1/Category/CategoryController.php
@@ -23,10 +23,14 @@ class CategoryController extends Controller
     {
         
         $categories = Category::withCount('subcategories');
+
+        $query = request()->query();
+
         if (request()->has('category_id')){
             $categories = $categories->where('category_id', request('category_id'));
         }else{
-            $categories = $categories->whereNull('category_id');
+            $categories = $categories->where('name', 'LIKE', '%' . $query['search'] . '%')
+                                     ->whereNull('category_id');
         }
 
         return $this->paginate($categories->get());
