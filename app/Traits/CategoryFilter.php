c902fb14bd2511edb664f67d0f638da5d597fd6c
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2022-01-19 16:31:57 +0800

    E_CLASSROOM-210 [BE] Category Filter Function (#42)
    * E_CLASSROOM-210 [BE] Category Filter Function
    
    * To view only the parent category
    
    * Rebase from develop
    
    * Change base on the comment

---
+++ app/Traits/CategoryFilter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+use App\Models\Quiz;
+use App\Models\User;
+
+trait CategoryFilter
+{
+    protected function filter($query)
+    {
+        $taken_quizzes_id = Quiz::join('quizzes_taken', 'quizzes.id', '=', 'quizzes_taken.quiz_id')
+                            ->where('quizzes_taken.user_id', Auth::user()->id)
+                            ->get()
+                            ->pluck('category_id')
+                            ->all();
+
+        if ($query['filter'] === "Taken"){
+            $categories = Category::wherein('id', $taken_quizzes_id);
+        } elseif ($query['filter'] === "Not Taken"){
+            $categories = Category::whereNotIn('id', $taken_quizzes_id);
+        }
+
+        $categories = $categories->whereNull('category_id')
+                                 ->orderBy('name', $query['sortBy'])
+                                 ->get();
+
+        return $categories;
+    }
+}
