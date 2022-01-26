f89a80edaf1458cdc53a29424b1852a24653a1e1
Joash Cañete <joash.canete@sun-asterisk.com>
2022-01-26 09:47:12 +0800

    E_CLASSROOM-215-FE/BE-Profile-Edit-Function-fully rebased

---
+++ app/Http/Controllers/API/v1/User/UploadImageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UploadImageRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UploadImageController extends Controller
+{
+    public function uploadImage(UploadImageRequest $request) 
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        $user->avatar = $user->name . '-' . time() . '.' . $request->image->extension();
+        $user->save();
+
+        $image = $user->name . '-' . time() . '.' . $request->image->extension();
+        
+        $request->image->move(public_path('avatar'), $image);
+    
+        return $this->successResponse('successfully uploaded', 200);
+    }
+}
