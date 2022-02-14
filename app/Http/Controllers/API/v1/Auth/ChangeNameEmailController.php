3c0b1c8c8a6a7879420a4aea4a20bd04159dbc09
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2022-02-14 18:46:49 +0800

    E_CLASSROOM-270 [Bugfix] [Student] [View Result] the failed score must be red and [Edit Account Info] must display error message

--- app/Http/Controllers/API/v1/Auth/ChangeNameEmailController.php
+++ app/Http/Controllers/API/v1/Auth/ChangeNameEmailController.php
@@ -13,20 +13,15 @@ class ChangeNameEmailController extends Controller
 {
     public function changeName(ChangeNameEmailRequest $request)
     {
-        $id = Auth::user()->id;
-        $user = User::find($id);
-        
+        $user = Auth::user();
+   
+        if (!Hash::check($request->password, $user->password)) {
+            return $this->errorResponse(['password' => 'The password you have entered is incorrect.'], 401);
+        }
+
         $user->name = $request['name'];
         $user->email = $request['email'];
         $user->save();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            if(!$user){
-                return $this->errorResponse(['error' => 'The email you have entered is incorrect.'], 401);
-            } else {
-                return $this->errorResponse(['error' => 'The password you have entered is incorrect.'], 401);
-            }
-        }
 
         return $this->successResponse('successfully changed', 200);
     }
