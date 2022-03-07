404798316709c91f284337656d6381d454760e46
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2022-03-07 14:46:33 +0800

    E_CLASSROOM-276 [FE/BE] Accounts List Page

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -17,12 +17,101 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
-            'user_type_id' => 1,
-            'avatar' => 'avatar.jpg'
-     ]);
+        $admins = [
+            [
+                'name' => 'John Doe',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Jane Doe',
+                'email' => 'janedoe@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Calvin Harris',
+                'email' => 'calvin@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Bruce Wayne',
+                'email' => 'bruce@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Tony Stark',
+                'email' => 'tonystark@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Mark Zuckerberg',
+                'email' => 'markz@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Charles Babbage',
+                'email' => 'charles@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Dennis Ritchie',
+                'email' => 'dennis@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Jon Bellion',
+                'email' => 'jon@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Ryan Reynolds',
+                'email' => 'ryanreynolds@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Peter Parker',
+                'email' => 'peterparker@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Stephen Strange',
+                'email' => 'strange@gmail.com@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ],[
+                'name' => 'Diana Prince',
+                'email' => 'dianaprince@gmail.com@gmail.com',
+                'password' => bcrypt('password'),
+                'user_type_id' => 1,
+                'avatar' => 'avatar.jpg',
+                'created_at' => date("Y-m-d H:i:s")
+            ]
+        ];
+        
+        DB::table('users')->insert($admins);
     }
 }
